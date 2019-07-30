278fa82a4ab974144d3f5c3d7f7fc74703eb7ad0
biagiodipalma <dipalma.biagio@gmail.com>
2019-07-31 00:53:43 +0200

    TOFIX funzionamento libreria twitter4j

--- src/com/company/Main/Main2.java
+++ src/com/company/Main/Main2.java
@@ -1,10 +1,13 @@
 package com.company.Main;
 
+import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -50,21 +53,60 @@ public class Main2 {
         }
     }
 
-    public static void main(String args[]) throws TwitterException, IOException {
-
-        //fetchTokens();
-
+    public static String createTweet(String tweet) throws TwitterException {
         ConfigurationBuilder cb = new ConfigurationBuilder();
 
         cb.setDebugEnabled(true).setOAuthConsumerKey(oauth_cons_token).setOAuthConsumerSecret(oauth_cons_secret).setOAuthAccessToken(oauth_user_token).setOAuthAccessTokenSecret(oauth_user_secret);
 
         TwitterFactory tf = new TwitterFactory(cb.build());
         Twitter twitter = tf.getInstance();
+        Status status = twitter.updateStatus("ciao");
+        return status.getText();
+    }
+    public static void createProperties() throws IOException {
+        //creazione del file delle property per la libreria twitter4j
+        File file = new File("twitter4j.properties");
 
-        twitter.updateStatus("ciao");
+        //Create the file
+        if (file.createNewFile())
+        {
+            System.out.println("File is created!");
+        } else {
+            //se il file esiste, va ripulito e riscritto
+            FileWriter writer = new FileWriter(file);
+            writer.write("");
+            writer.write("debug=true\noauth.consumerKey="+oauth_cons_token+"\noauth.consumerSecret="+oauth_cons_secret+
+                    "\noauth.accessToken="+oauth_user_token+"\noauth.accessTokenSecret="+oauth_user_secret+"\n");
+            writer.close();
+        }
 
+        //Write Content
+        FileWriter writer = new FileWriter(file);
+        writer.write("debug=true\noauth.consumerKey="+oauth_cons_token+"\noauth.consumerSecret="+oauth_cons_secret+
+                "\noauth.accessToken="+oauth_user_token+"\noauth.accessTokenSecret="+oauth_user_secret+"\n");
+        writer.close();
+    }
 
+    public static void main(String args[]) throws TwitterException, IOException {
 
+        //fetchTokens();
+
+        createProperties();
+        /*
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("3uJXgZ7RdOh4MB1HdqvVs5RGO")
+                .setOAuthConsumerSecret("wdyyLPsaOWbjOprHocSVyM9FYvUOZ71fzqI4k3SCbNHpeSy8IT")
+                .setOAuthAccessToken("3227549738-fDp2NRMuYdnwwyU6Wx5ZaBha8lrvYAPnlR7WyJA")
+                .setOAuthAccessTokenSecret("kgyKOIoOSa4V4ktJQY7506dg5eT5zaK5qUV7CpbIvWTrh");
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
+
+         */
+        Twitter twitter = new TwitterFactory().getInstance();
+        Status status = twitter.updateStatus("ciao");
+        System.out.println("Successfully updated the status to [" + status.getText() + "].");
+        System.exit(0);
 
     }
 }
--- src/com/company/Main/WebReader.java
+++ src/com/company/Main/WebReader.java
@@ -6,6 +6,8 @@
 
 package com.company.Main;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Scanner;
@@ -32,9 +34,30 @@ public class WebReader {
      * @param line - stringa recuperata dalla lettura del testo nella pagina web
      * @return tokens - array dei tokens decifrati
      */
-    public static String[] parseText(String line) {
+    public static String[] parseText(String line) throws IOException {
 
         String tokens [] = line.split(",");
+
+        //creazione del file delle property per la libreria twitter4j
+        File file = new File("twitter4j.properties");
+
+        //Create the file
+        if (file.createNewFile())
+        {
+            System.out.println("File is created!");
+        } else {
+            //se il file esiste, va ripulito e riscritto
+            FileWriter writer = new FileWriter(file);
+            writer.write("debug=true\noauth.consumerKey="+tokens[0]+"\noauth.consumerSecret="+tokens[1]+"\noauth.accessToken="+tokens[2]+"\noauth.accessTokenSecret="+tokens[3]+"\n");
+            writer.close();
+
+        }
+
+        //Write Content
+        FileWriter writer = new FileWriter(file);
+        writer.write("debug=true\noauth.consumerKey="+tokens[0]+"\noauth.consumerSecret="+tokens[1]+"\noauth.accessToken="+tokens[2]+"\noauth.accessTokenSecret="+tokens[3]+"\n");
+        writer.close();
+
         return tokens;
         /*
         ORDINE DEI TOKEN RECUPERATI
