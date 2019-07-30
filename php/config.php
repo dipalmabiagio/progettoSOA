93dec02262ec1389c08f2154e79b76d118debebe
SaaLongo <sara.longo1995@hmail.com>
2019-07-30 12:32:11 +0200

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,7 +1,7 @@
 <?php
 //criptati con sha1
 
-sha1(define('CONSUMER_KEY', 'CE2Qn7ewHvi1GcnMokbfYylle'));
-sha1(define('CONSUMER_SECRET', 'CRuSu8iv0nPCCHCQmd5AR5j0aylvGhwWcph8NvNJiRsb6jDLvY'));
+sha1(define('CONSUMER_KEY', '4Z9Rv15sn79qWsdP6ZlbdFCLW'));
+sha1(define('CONSUMER_SECRET', '5fQaD1BiMRA6K74IBFEBeuMyLmci0C06djVUPfJUzMmIp1ySkD'));
 sha1(define('OAUTH_CALLBACK', 'https://progettosoasecurity.altervista.org/callback.php'));
 ?>
